92ad092d6fbb9a0dec27c16d3e550549584290a4
Carlie16604 <carliemaloneco@gmail.com>
2023-10-23 23:45:06 -0600

    first commit

---
+++ puppy-react/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import {puppyList} from './data.js'
+
+function App() {
+
+}
+
+export default App
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import AboutMe from './AboutMe';
+
+function App() {
+    return (
+      <div>
+        <nav className='navBar'>
+          <p>About Me</p>
+          <p>Projects</p>
+          <p>Contact</p>
+        </nav>
+        <div>
+          {/* <img src="./Public/Black_Sig.png" className={signatures.logo} alt="Black Signature" /> */}
+        </div>
+          <p>bro?</p>
+      </div>
+    );
+  };
+
+export default App;
