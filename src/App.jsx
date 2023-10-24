771e1bf2710b676d09652f635b4a8952dd4c6927
Carlie16604 <carliemaloneco@gmail.com>
2023-10-24 15:14:09 -0600

    styling updates & links added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
-import Black_Sig from "./imgs/Black_Sig.png";
-import './App.css'
+import White_Sig from "./imgs/White_Sig.png";
+import Github from "./imgs/Github.png";
+import LinkedIn from "./imgs/LinkedIn.png";
+import './App.css';
+import Home from './Home.jsx';
+import './Home.css';
 // import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 // import AboutMe from './AboutMe';
 
 function App() {
     return (
-      <div>
+      <>
         <nav className='navBar'>
-        <img src={Black_Sig} className="App-logo" alt="Black Signature" />
-          <p>About Me</p>
-          <p>Projects</p>
-          <p>Contact</p>
+          <img src={White_Sig} className="App-logo" alt="White Signature" />
         </nav>
-        <div>
-          <h1>Hello</h1>
+        <p className='contact'>CONTACT</p>
+        <div className='sideBar'>
+          <a href='https://github.com/Carlie16604'>
+            <img src={Github} className='logo-size' alt="Github"/>
+          </a>
+          <a href='https://www.linkedin.com/in/carlie-malone23/'>
+            <img src={LinkedIn} className='logo-size' alt="LinkedIn" />
+          </a>
         </div>
-      </div>
+        <Home />
+      </>
     );
   };
 
