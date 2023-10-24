be5a37f24b457e72563a1d5ddec4e2141ea4924a
Carlie16604 <carliemaloneco@gmail.com>
2023-10-24 15:46:55 -0600

    added rotating titles and name; working on positioning

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,12 +3,16 @@ import './Home.css'
 const Home = () => {
     return (
     <>
-        <h1 className='titles'></h1>
+    <div>
+        <h1 className='name'>Carlie Malone</h1>
+        <h1 className='titles'>
             <div>?</div>
             <span>FullStack Developer</span>
             <span>UX Developer</span>
             <span>Professional Mucisian</span>
             <span>Leader</span>
+        </h1>
+    </div>
     </>
     );
 };
