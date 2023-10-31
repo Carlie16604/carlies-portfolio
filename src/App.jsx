23a111078046cd6c87ae3b7e6c0b51cdb9b5b887
Carlie16604 <carliemaloneco@gmail.com>
2023-10-31 15:39:36 -0600

    added Contact page, working on styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import ReactDOM from "react-dom/client";
-import React, {useEffect, useRef} from "react";
+import React from "react";
 import './App.css';
 import './Projects.css';
 import Black_Sig from "./imgs/Black_Sig.png";
@@ -15,6 +14,7 @@ import JS from './imgs/JavaScript-logo.png';
 import Titles from './Titles.jsx';
 import About from './AboutMe.jsx';
 import Projects from './Projects.jsx';
+import Contact from './Contact.jsx';
 
 function App() {
   // const toggleButton = document.querySelector('#theme-toggle');
@@ -44,7 +44,7 @@ function App() {
               <a href="#home">Home</a>
               <a href="#about">About</a>
               <a href="#projects">Projects</a>
-              {/* <a href="#contact">Contact</a> */}
+              <a href="#contact">Contact</a>
           </section>
         </nav>
       <div className='sideBar'>
@@ -81,6 +81,7 @@ function App() {
       </div>
       <About />
       <Projects />
+      <Contact />
     </div>
     </>
   ) 
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,3 +1,24 @@
+import React from 'react';
+import mail from './imgs/mail.png';
+import './contact.css';
+import './About.css';
+
 const Contact = () => {
-    
+    return (
+        <>
+            <section id='contact' className='contact'>
+                <div className='cont-container'> 
+                    <h1 className='cont-title'>Contact</h1>
+                        <h3>Feel free to reach out to me!</h3>
+                            <div className='cont-info'>
+                                <img src={mail} className='logo-setting' alt='Mail PNG' />
+                                <p>Email: carliemaloneco@gmail.com</p>
+                            </div>
+                    {/* <p>Location: Broomfield, CO?</p> */}
+                </div>
+            </section>
+        </>
+    )
 }
+
+export default Contact
