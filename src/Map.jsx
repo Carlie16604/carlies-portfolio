61ca4d03c5bbb736e69e9eab2fc08d1a93f80d28
Carlie16604 <carliemaloneco@gmail.com>
2023-11-20 13:23:16 -0700

    testing the media tag

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+//potentially used for a google map functionality
+
 class Map extends Component{
     componentDidMount(){
       const mapDiv = this.refs.map;
