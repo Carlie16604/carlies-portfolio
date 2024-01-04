583cf6f25617b1efca88800a46da8adbd3e44711
Carlie Malone <carliemaloneco@gmail.com>
2024-01-04 14:26:29 -0700

    fixed max-widths

--- src/Titles.jsx
+++ src/Titles.jsx
@@ -2,16 +2,16 @@ import './titles.scss'
 
 const Titles = () => {
     return (
-    <>
-    <div>
-        <h1 className='titles'>
-            <span>FullStack Developer</span>
-            <span>Professional Musician</span>
-            <span>UX / UI Developer</span>
-            <span>Problem Solver</span>
-        </h1>
-    </div>
-    </>
+        <>
+            <div>
+                <h1 className='titles'>
+                    <span>FullStack Developer</span>
+                    <span>Professional Musician</span>
+                    <span>UX / UI Developer</span>
+                    <span>Problem Solver</span>
+                </h1>
+            </div>
+        </>
     );
 };
 
