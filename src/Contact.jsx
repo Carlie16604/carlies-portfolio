7af63c6e214daf402abea72d40c800b89db1b27e
Carlie Malone <carliemaloneco@gmail.com>
2024-01-15 14:29:52 -0700

    working on contact page bug

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,28 +6,27 @@ import './scss/contact.scss';
 const Contact = () => {
     return (
         <>
-            <section id='contact' className='contact'>
+            <div id='contact' className='contact'>
                 <div className='containerContact'> 
                     <div className='cont-content'>
                         <div className='cont-title'>
                             <p>Contact</p>
                             <h3>Feel free to reach out to me!</h3>
                         </div>
-                    <div className='cont-icons'>
-                        <img src={mail} className='cont-logo-setting' alt='Mail svg' />
-                            <div className='cont-info'>
-                                <a className='portal' href='mailto:carliemaloneco@gmail.com'>carliemaloneco@gmail.com</a>
-                            </div>
-                        <img src={LinkedIn} className='cont-logo-setting' alt='LinkedIn svg' />
-                            <div className='cont-info'>
-                                <a className='portal' href='https://www.linkedin.com/in/carlie-malone23/' target='_blank'>Carlie Malone</a>
-                            </div>
-                        {/* <img src={GitHub} className='cont-logo-setting' alt='GitHub PNG' /> */}
+                        <div className='cont-icons'>
+                            <img src={mail} className='cont-logo-setting' alt='Mail svg' />
+                                <div className='cont-info'>
+                                    <a className='portal' href='mailto:carliemaloneco@gmail.com'>carliemaloneco@gmail.com</a>
+                                </div>
+                            <img src={LinkedIn} className='cont-logo-setting' alt='LinkedIn svg' />
+                                <div className='cont-info'>
+                                    <a className='portal' href='https://www.linkedin.com/in/carlie-malone23/' target='_blank'>Carlie Malone</a>
+                                </div>
+                            {/* <img src={GitHub} className='cont-logo-setting' alt='GitHub PNG' /> */}
                     </div>
                 </div>
-                    {/* <p>Location: Broomfield, CO?</p> */}
             </div>
-            </section>
+        </div>
         </>
     )
 }
