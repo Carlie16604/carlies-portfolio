3e9aa6bea558083cfde7168f27b117a7f250239c
Carlie <carliemaloneco@gmail.com>
2024-07-22 22:13:17 -0600

    added tailwind

--- src/Main.jsx
+++ src/Main.jsx
@@ -45,10 +45,10 @@ function App() {
                   <img src="https://skillicons.dev/icons?i=js,nodejs,postgres" alt='javacript, nodejs, and Postgres' title='Javascript, Node.js, and Postgres'/>
                 </li>
                 <li>
-                  <img src="https://skillicons.dev/icons?i=postman,sass,figma" alt='Postman, Figma, & Sass logo' title='Postman, SASS, and Figma' />
+                  <img src='https://skillicons.dev/icons?i=react,vite,tailwind' alt='react, vite, & dotnet logo' title='React, Vite, and DotNet' />
                 </li>
                 <li>
-                  <img src='https://skillicons.dev/icons?i=react,vite,dotnet' alt='react, vite, & dotnet logo' title='React, Vite, and DotNet' />
+                  <img src="https://skillicons.dev/icons?i=figma,sass,postman" alt='Postman, Figma, & Sass logo' title='Postman, SASS, and Figma' />
                 </li>
               </ul>
             </div>
