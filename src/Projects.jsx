ed82b063587f239549b34c154c8fde0e164b070f
Carlie16604 <carliemaloneco@gmail.com>
2024-08-20 12:48:47 -0600

    updating links & visability

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -40,11 +40,11 @@ const Projects = () => {
                                     <img src={github} className='pro-setting' title='See the code' alt='github icon'/>
                                     <p>The Code</p>
                                 </a>
-                                <a href='https://github.com/Carlie16604/Elevate' target='_blank' rel='noreferrer'>
+                                <a href='https://elevate-pilates.netlify.app' target='_blank' rel='noreferrer'>
                                     <img src={rocketship} className='pro-setting' title='View the Website' alt='preview icon'/>
                                     <p>Live Demo</p>
                                 </a>
-                                <a href='https://github.com/Carlie16604/Elevate' target='_blank' rel='noreferrer'>
+                                <a href='https://www.figma.com/design/QBaaWmJwpHgEG38mw4zpqD/Pilates?node-id=0-1&t=kcBGTzCMwBPg0jTl-0' target='_blank' rel='noreferrer'>
                                     <img src={figma} className='pro-setting' title='View the Website' alt='Figma icon'/>
                                     <p>Design</p>
                                 </a>
