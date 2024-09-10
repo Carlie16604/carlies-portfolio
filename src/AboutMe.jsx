e489060d93a0d9270cc6aa9902bc8295fc02255e
Carlie <carliemaloneco@gmail.com>
2024-09-10 16:23:20 -0600

    cleaning up + about section style update

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className='container3'>
           <div className='about-content'>
             <img className='img-about' src={Desktop} alt='Desktop img'/> 
-                <div>
+                <div className='aboutInfo'>
                     <h4 className='text-title'>About Me</h4>
                     <p className='text-about'>As a Junior Fullstack Developer, I bring a diverse skill set to the table, including proficiency in HTML, CSS, JavaScript, React, Node.js, PostgresQL, Express, and SASS. My strengths lie with creating and maintaining responsive websites that deliver exceptional user experiences.</p>
                     <p className='text-about'>I specialize in designing dynamic and captivating interfaces, employing clean and efficient coding practices, and harnessing the latest development tools and methodologies. Additionally, I am a collaborative team player, thriving in cross-functional environments and contributing to the development of outstanding web applications.</p>
